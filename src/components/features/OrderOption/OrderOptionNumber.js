876307d08d395cbee7a9382cd04f203ad0118373
Paweł Hoła <phola85@wp.pl>
2020-01-18 23:40:11 +0100

    Add order option number to project

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+import { formatPrice } from '../../../utils/formatPrice';
 
-const OrderOptionNumber = () => (
+const OrderOptionNumber = ({ currentValue, setOptionValue, limits, price }) => (
 
-  <div>OrderOptionNumber</div>
+  <div className={styles.number}>
+    <input
+      type='number'
+      className={styles.inputSmall}
+      value={currentValue}
+      min={limits.min}
+      max={limits.max}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+    />
+    ({formatPrice(price)})
+  </div>
 );
+
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.node,
+  setOptionValue: PropTypes.func,
+  limits: PropTypes.object,
+  price: PropTypes.string,
+};
 
 export default OrderOptionNumber;
