f0dffd662cf76cb139efa51781a550be0240d469
Paweł Hoła <phola85@wp.pl>
2020-01-25 21:52:10 +0100

    Add optionTypes, mockProps to tests

---
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import OrderOption from './OrderOption';
+
+describe('Component OrderOption', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<OrderOption type={'type'} name={'name'} id={'id'} />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should return empty object if called without required props', () => {
+    const component = shallow (<OrderOption />);
+    expect(component).toEqual({});
+  });
+
+  it('should redner title', () => {
+    const expectedTitle = 'title';
+    const component = shallow(<OrderOption type={'type'} name={expectedTitle} id={'id'} />);
+
+    expect(component.find('.title').text()).toEqual(expectedTitle);
+    console.log(component.debug());
+  });
+
+  const optionTypes = {
+    dropdown: 'OrderOptionDropdown',
+    icons: 'OrderOptionIcons',
+    checkboxes: 'OrderOptionCheckboxes',
+    number: 'OrderOptionNumber',
+    text: 'OrderOptionText',
+    date: 'OrderOptionDate',
+  };
+
+  const mockProps = {
+    id: 'abc',
+    name: 'Lorem',
+    values: [
+      {id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0},
+      {id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100},
+    ],
+    required: false,
+    currentValue: 'aaa',
+    price: '50%',
+    limits: {
+      min: 0,
+      max: 6,
+    },
+  };
+
+  const mockPropsForType = {
+    dropdown: {},
+    icons: {},
+    checkboxes: {currentValue: [mockProps.currentValue]},
+    number: {currentValue: 1},
+    text: {},
+    date: {},
+  };
+
+  const testValue = mockProps.values[1].id;
+  const testValueNumber = 3;
+
+  for(let type in optionTypes){
+    describe('Component OrderOption with type=${type}', () => {
+      /* test setup */
+      let component;
+      let subcomponent;
+      let renderedSubcomponent;
+
+      beforeEach(() => {
+        component = shallow (
+          <OrderOption
+            type={type}
+          />
+        );
+        subcomponent = component.find(optionTypes[type]);
+        renderedSubcomponent = subcomponent.dive();
+      });
+
+      /* common tests */
+      it('passes dummy test', () => {
+        expect(1).toBe(1);
+        console.log(component.debug());
+      });
+
+      /* type-specific tests */
+      switch (type) {
+        case 'dropdown': {
+          /* tests for dropdown */
+          break;
+        }
+      }
+    });
+  }
+});
